d51883fc539d10dd729c0680517a491d863e4496
stackviolator <joshmerrill255@gmail.com>
2024-02-29 21:03:46 -0500

    disable services

--- red-scripts/disable_service.sh
+++ red-scripts/disable_service.sh
@@ -3,17 +3,13 @@
 SERVICE_NAME="sshd" #CHANGE ME
 
 disable_service() {
-    echo "Attempting to disable ${SERVICE_NAME}..."
     sudo systemctl stop "$SERVICE_NAME"
     sudo systemctl disable "$SERVICE_NAME"
 }
 
 while true; do
     if systemctl is-active --quiet "$SERVICE_NAME"; then
-        echo "${SERVICE_NAME} is still active. Disabling..."
         disable_service
-    else
-        echo "${SERVICE_NAME} is disabled."
     fi
     sleep 5
 done
